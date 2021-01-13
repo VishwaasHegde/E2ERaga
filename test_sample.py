3f900dca2b898167f7291f6a45e74aa2b53a9832
Vishwaas <vishwaas.universe@gmail.com>
2021-01-13 21:43:13 +0530

    added cqt and predict_run_time

--- test_sample.py
+++ test_sample.py
@@ -4,7 +4,10 @@ if __name__ == '__main__':
     # import os
     # filename = [os.path.join(os.path.dirname(__file__), 'tests/sample_data/gap_2.wav')]
     # run(filename, viterbi=False)
-    core.train(task='raga', tradition='hindustani')
+    # core.train(task='raga', tradition='hindustani')
     # core.train(task='tonic', tradition='hindustani')
     # core.test(task='raga', tradition='hindustani')
+    # core.test_pitch(task='pitch', tradition='hindustani')
     # core.train_pitch(tradition='hindustani')
+    # core.cache_cqt(task='raga', tradition='hindustani')
+    core.predict_run_time(tradition='hindustani')
