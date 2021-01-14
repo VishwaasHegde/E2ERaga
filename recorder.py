65716f64833ab01a8bb72085c7a523a22680f9d1
Vishwaas <vishwaas.universe@gmail.com>
2021-01-14 12:41:13 +0530

    updates for cmd line args

--- recorder.py
+++ recorder.py
@@ -5,6 +5,9 @@ fs = 16000  # Sample rate
 
 
 def record(seconds):
+    key = input('Press 1 to start recording or press 0 to exit: ')
+    if key == str(0):
+        return None
     print('Started recording for {} seconds'.format(seconds))
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
     sd.wait()  # Wait until recording is finished
